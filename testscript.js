6a9f7f6317b2be888c12702c653d62031a5f0de7
Usmar A Padow <usmpadow@gmail.com>
2015-02-26 20:07:39 +0900

    Added a statement for the GPL

--- testscript.js
+++ testscript.js
@@ -1,4 +1,22 @@
 //Copiright Usmar A Padow (amigojapan) usmpadow@gmail.com 2012 - 2015 Released as GPL LGPL in 2015 read COPYING.TXT for details
+/*
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/*
+
+
+
+
 //perhaps change the implementation so I can stop using quotes using teh getcomponent like this theCamera = GetComponent("MyCamera");
 /* 
 //implement later
